bb933d537dad5600214f91b6c7bab39f39d9ad2d
lexcf <77665926+lexcf@users.noreply.github.com>
2022-04-21 19:20:52 +0300

    Update keywords.h

--- keywords.h
+++ keywords.h
@@ -2,5 +2,7 @@
 
 typedef void(*OnClientConnectCallback)();
 typedef void(*OnDataReceivedCallback)(char*);
+typedef void(*FPC_CALLBACK) (SOCKET clientSocket, addrinfo* addressInfo);
+typedef void(*FPS_CALLBACK) (SOCKET clientSocket, addrinfo* addressInfo, char* data);
 
 #define DEFAULT_BUFF 1024
