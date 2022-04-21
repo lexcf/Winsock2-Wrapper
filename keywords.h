e8427e500e96125631fb2b71e51f89ba3b76367e
lexcf <77665926+lexcf@users.noreply.github.com>
2022-04-21 20:51:55 +0300

    Delete keywords.h

--- keywords.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-typedef void(*OnClientConnectCallback)();
-typedef void(*OnDataReceivedCallback)(char*);
-typedef void(*FPC_CALLBACK) (SOCKET clientSocket, addrinfo* addressInfo);
-typedef void(*FPS_CALLBACK) (SOCKET clientSocket, addrinfo* addressInfo, char* data);
-
-#define DEFAULT_BUFF 1024
