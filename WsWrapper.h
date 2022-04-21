b8a1efdd674677cfdc773590b6b208137f40c042
lexcf <77665926+lexcf@users.noreply.github.com>
2022-04-21 19:20:35 +0300

    Update WsWrapper.h

--- WsWrapper.h
+++ WsWrapper.h
@@ -29,6 +29,39 @@ namespace WsWrapper {
 		void close();
 	};
 
+	class Server
+	{
+	public:
+		SOCKET Socket_Listen;
+		SOCKET Socket_Client;
+
+	public:
+		bool start_server(PCSTR port, int protocol, FPC_CALLBACK fpConnected = 0, FPS_CALLBACK fpData = 0, bool thread = true);
+		void send_data(SOCKET clientSocket, const char* sendChar);
+		void close();
+	};
+
+	DWORD WINAPI			__t__listenThread(LPVOID lpParams);
+	DWORD WINAPI			__t__startserver(LPVOID lpParams);
+	DWORD WINAPI			__t__createServerRes(LPVOID lpParams);
+	bool __createserver(WsWrapper::Server* const& server_class, PCSTR port, int protocol, FPC_CALLBACK fpConnected, FPS_CALLBACK fpData);
+
+	struct s_server_params
+	{
+		PCSTR			port;
+		int				protocol;
+		FPC_CALLBACK	fpConnected;
+		FPS_CALLBACK	fpData;
+		SOCKET			socket_l;
+		SOCKET			socket_c;
+		WsWrapper::Server* s_class;
+	};
+
+	struct s_server_res {
+		SOCKET SocketResponse;
+		FPS_CALLBACK fpResData;
+		struct addrinfo* addrRes;
+	};
 }
 
 #undef _CRT_SECURE_NO_WARNINGS
